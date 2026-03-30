142fb360d4b637d69e255d3e4f0ce98c76409ee0
Pagarizantali <egor.igorevitch2002@yandex.ru>
2026-03-31 03:59:37 +0500

    Task2

---
+++ Tyuiu.SavchenkoEI.Sprint1.Task2.V30.Lib/DataService.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.SavchenkoEI.Sprint1.Task2.V30.Lib
+{
+    public class DataService : ISprint1Task2V30
+    {
+        public double ConvertKmToMetre(double value)
+        {
+            return Math.Round(value * 1000, 3);
+        }
+    }
+}
---
+++ Tyuiu.SavchenkoEI.Sprint1.Task2.V30.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.SavchenkoEI.Sprint1.Task2.V30.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
