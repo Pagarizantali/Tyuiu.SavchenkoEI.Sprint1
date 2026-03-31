1fdfd49af17e8feb3a6039e5f685865fe717e717
Pagarizantali <egor.igorevitch2002@yandex.ru>
2026-03-31 05:06:25 +0500

    Task6

---
+++ Tyuiu.SavchenkoEI.Sprint1.Task6.V13.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.SavchenkoEI.Sprint1.Task6.V13.Lib
+{
+    public class DataService : ISprint1Task6V13
+    {
+        public bool CheckWordsAlphabet(string value)
+        {
+            for (int i = 1; i < value.Length; i++)
+            {
+                if (value[i] < value[i - 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Tyuiu.SavchenkoEI.Sprint1.Task6.V13.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.SavchenkoEI.Sprint1.Task6.V13.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
